54b2cd5dc297f8bdeb4b568f25c2510160f69f40
ndduy3001 <ndduy0685@gmail.com>
2025-11-09 22:44:50 +0700

    feat: add feature 'quizzes'

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -1,14 +1,24 @@
 from django.urls import path
 from .views import (
-    QuizDetailView,
+    QuizListCreateView,
+    QuizRetrieveUpdateDelete,
     AttemptStartView,
     AttemptSubmitView,
     AttemptResultView,
+    QuestionListCreateView,
+    QuestionRetrieveUpdateDelete,
+    ChoiceListCreateView,
+    ChoiceRetrieveUpdateDelete
 )
 
 urlpatterns = [
-    path("quizzes/<uuid:lesson_id>/", QuizDetailView.as_view()),
-    path("quizzes/<uuid:lesson_id>/attempts/", AttemptStartView.as_view()),
-    path("quizzes/attempts/<uuid:attempt_id>/submit/", AttemptSubmitView.as_view()),
-    path("quizzes/attempts/<uuid:id>/", AttemptResultView.as_view()),
+    path("quizzes/", QuizListCreateView.as_view()),
+    path("quiz/<uuid:lesson_id>/", QuizRetrieveUpdateDelete.as_view()),
+    path("quiz/<uuid:quiz_id>/questions", QuestionListCreateView.as_view()),
+    path("quiz/question/<uuid:id>" ,QuestionRetrieveUpdateDelete.as_view()),
+    path("question/<uuid:question_id>/choices" ,ChoiceListCreateView.as_view()),
+    path("choice/<uuid:id>/", ChoiceRetrieveUpdateDelete.as_view()),
+    path("quiz/<uuid:lesson_id>/attempts/", AttemptStartView.as_view()),
+    path("quiz/attempts/<uuid:attempt_id>/submit/", AttemptSubmitView.as_view()),
+    path("quiz/attempts/<uuid:id>/", AttemptResultView.as_view()),
 ]
